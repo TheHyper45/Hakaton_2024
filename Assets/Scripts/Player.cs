02ad4f0c6ea307c42e001154a2afaaf13f562bfd
TheHyper45 <dawid.priv@o2.pl>
2024-11-30 11:40:55 +0100

    Camera pos lerping.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,18 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     [SerializeField]
     private float speed;
+    [SerializeField]
+    private new Camera camera;
+    [SerializeField]
+    private float cameraLerpSpeed;
 
     private void Update() {
+        camera.transform.position = new Vector3(
+            Mathf.Lerp(camera.transform.position.x,transform.position.x,cameraLerpSpeed * Time.deltaTime),
+            Mathf.Lerp(camera.transform.position.y,transform.position.y,cameraLerpSpeed * Time.deltaTime),
+            camera.transform.position.z
+        );
+
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
             transform.position += speed * Time.deltaTime * new Vector3(0.0f,1.0f,0.0f);
         }
